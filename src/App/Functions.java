6c683ddf37a04178325c6d2ef0b6daed02774943
3vinicius <amorimvinicius3@gmail.com>
2023-05-25 16:12:45 -0300

    new commit

--- src/App/Functions.java
+++ src/App/Functions.java
@@ -1,4 +1,16 @@
 package App;
 
 public class Functions {
+    public static void main(String[] args){
+        int num = 20;
+        System.out.println(fatorial(num));
+    }
+
+    public static long fatorial(int num){
+        long result = 1;
+        for(int i = 1; i <= num; i++ ){
+            result *= i;
+        }
+        return result;
+    }
 }
